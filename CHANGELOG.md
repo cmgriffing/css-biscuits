c4c4ac84938edf220aadd44ec80f9236ec88c7da
Chris Griffing <cmgriffing@gmail.com>
2020-10-16 13:31:48 -0700

    feat: initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,21 @@
+# Change Log
+
+All notable changes to the "html-biscuits" extension will be documented in this file.
+
+Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
+
+### 0.0.5
+
+Handle top level elements better
+
+### 0.0.3
+
+Handle "last element in parent" situations differently than siblings
+
+### 0.0.2
+
+Handle nested values better
+
+### 0.0.1
+
+First tested public launch
---
+++ src/activate.ts
@@ -0,0 +1,78 @@
+import vscode, { Range, TextDocument as RawTextDocument } from "vscode";
+import { createActivate } from "biscuits-base";
+
+import * as cssCore from "vscode-css-languageservice";
+import { FoldingRange } from "vscode-css-languageservice";
+
+// Needs to be genericized
+const CONFIG_PREFIX_KEY = "css-biscuits.annotationPrefix";
+const CONFIG_COLOR_KEY = "css-biscuits.annotationColor";
+const CONFIG_DISTANCE_KEY = "css-biscuits.annotationMinDistance";
+const CONFIG_MAX_LENGTH = "css-biscuits.annotationMaxLength";
+
+export const activate = createActivate(
+  CONFIG_COLOR_KEY,
+  CONFIG_DISTANCE_KEY,
+  CONFIG_PREFIX_KEY,
+  {
+    createDecorations(
+      text: string,
+      activeEditor: vscode.TextEditor,
+      prefix: string,
+      minDistance: number
+    ) {
+      // const sourceFile = ts.createSourceFile(
+      //   "currentFile",
+      //   newText,
+      //   ts.ScriptTarget.Latest
+      // );
+      console.log("Language", activeEditor.document.languageId);
+
+      const rawTextDocument = { ...activeEditor.document } as any;
+      rawTextDocument.uri = rawTextDocument.uri.toString();
+      const textDocument = rawTextDocument as cssCore.TextDocument;
+
+      let css = cssCore.getSCSSLanguageService();
+      if (activeEditor.document.languageId === "less") {
+        css = cssCore.getLESSLanguageService();
+      }
+      const stylesheet = css.parseStylesheet(textDocument);
+      const ranges = css.getFoldingRanges(textDocument);
+
+      const decorations: any[] = [];
+
+      ranges.forEach((range: FoldingRange) => {
+        const endLine = range.endLine + 1;
+        const startLine = range.startLine;
+
+        let contentText = activeEditor.document.lineAt(startLine).text.trim();
+
+        if (contentText.charAt(0) === "{") {
+          contentText = activeEditor.document.lineAt(startLine - 1).text.trim();
+        }
+        console.log("last char", contentText.charAt(contentText.length - 1));
+        if (contentText.charAt(contentText.length - 1) === "{") {
+          contentText = contentText.slice(0, contentText.length - 2);
+        }
+
+        const endOfLine = activeEditor.document.lineAt(endLine).range.end;
+
+        if (endLine - startLine >= minDistance) {
+          decorations.push({
+            range: new vscode.Range(
+              activeEditor.document.positionAt(endLine),
+              endOfLine
+            ),
+            renderOptions: {
+              after: {
+                contentText: `${prefix} ${contentText}`,
+              },
+            },
+          });
+        }
+      });
+
+      return decorations;
+    },
+  }
+);
